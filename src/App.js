c7e92f7c4654103c0dbbff4f1c4fb71269521bf5
DESKTOP-VHLAITO\mert1 <mert172317@gmail.com>
2022-06-25 00:29:10 +0300

    "number, string, object and array " added.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import './App.css'
 
 function App() {
 
-  const [number,setNumber] =useState(20)
+  const [number, setNumber] = useState(20)
+  const [name, setName] = useState("Mert")
+  const [isActive, setIsActive] = useState(false)
+  const [user, setUser] = useState({ name: "", lastname: "" })
+  const [numberArray, setNumberArray] = useState([1, 2, 3, 4, 5])
 
-  const changeNumber=() => {
+  const changeNumber = () => {
     const n = number + 10
     setNumber(n)
-    console.log("n",n)
+    console.log("n", n)
   }
 
   return (
     <div>
       <h1>Kişinin Yaşı {number}</h1>
       <button onClick={changeNumber}>Buton</button>
+      <h1>İsim Değeri {name}</h1>
+      <input value={name} onChange={(e) => { setName(e.target.value) }} />
+
+      <h1>{JSON.stringify(isActive)}</h1>
+      <input type="checkbox" checked={isActive} onChange={(e) => {
+        setIsActive(e.target.checked)
+      }} />
+
+      <h1>{JSON.stringify(user)}</h1>
+      <button onClick={() => {
+        setUser({ name: "Mert", lastname: "Kamalı" })
+      }}>User Bilgilerini Ata</button>
+      <h1>{JSON.stringify(numberArray)}</h1>
+
+      <button onClick={() => {
+        const arr = [...numberArray]
+        arr.push(10)
+        setNumberArray(arr)
+      }}>Eleman Ekle</button>
     </div>
   );
 }
